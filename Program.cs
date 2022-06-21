dc4bc002dfc227094b972c22c4c88fec2383c53c
Ignacio Garcia Vazquez <natxoga@gmail.com>
2022-06-21 08:04:24 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace T03_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 12;
+            double N = 6.5;
+            double M = 11.3;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(N);
+            Console.WriteLine(M);
+            Console.WriteLine(x+y);
+            Console.WriteLine(x-y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);
+            Console.WriteLine(x % y);
+            Console.WriteLine(N + M);
+            Console.WriteLine(N - M);
+            Console.WriteLine(N * M);
+            Console.WriteLine(N / M);
+            Console.WriteLine(N % M);
+            Console.WriteLine(x + N);
+            Console.WriteLine(y / M);
+            Console.WriteLine(y % M);
+
+            Console.WriteLine(x * 2);
+            Console.WriteLine(y * 2);
+            Console.WriteLine(N * 2);
+            Console.WriteLine(M * 2);
+            Console.WriteLine(x+y+N+M);
+            Console.WriteLine(x * y * N * M);
+
+            Console.ReadKey();
+        }
+    }
+}
